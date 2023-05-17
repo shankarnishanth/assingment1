87def74b763ba57bf0d28e89626b54a3c1bdf549
shankarnishanth <nishanth23shankar@gmail.com>
2023-05-17 10:32:17 +0530

    added:primitive program

---
+++ src/primitive.java
@@ -0,0 +1,21 @@
+public class primitive {
+    public static void main(String[] args) {
+        int  a =1444567;
+        float height =17.9f;
+        char letter = 'a' ;
+        byte b=117;
+        short c =-20000;
+        long d =234567622;
+        boolean flag = true;
+        double e =222.5;
+        System.out.println(a);
+        System.out.println(height);
+        System.out.println(letter);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(flag);
+        System.out.println(e);
+
+    }
+}
