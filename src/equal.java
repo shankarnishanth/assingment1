396f8189c67def280a60f525044742c41e17ab1d
shankarnishanth <nishanth23shankar@gmail.com>
2023-05-17 10:44:10 +0530

    added:message program

---
+++ src/equal.java
@@ -0,0 +1,12 @@
+public class equal {
+    public static void main(String[] args) {
+        String a = "hello";
+        String b = "hello";
+        if (a == b) {
+            System.out.println("2 string are equal");
+        } else {
+            System.out.println("2 string are not equal");
+        }
+    }
+}
+
